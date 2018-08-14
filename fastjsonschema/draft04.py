fc5467d79f6ef36bdd332c88649c9a5f4bd570d4
Michal Horejsek <michal.horejsek@nic.cz>
2018-08-14 10:50:48 +0200

    Boolean in items

--- fastjsonschema/draft04.py
+++ fastjsonschema/draft04.py
@@ -278,11 +278,35 @@ class CodeGeneratorDraft04(CodeGenerator):
             self.l('raise JsonSchemaException("{name} must contain unique items")')
 
     def generate_items(self):
+        """
+        Means array is valid only when all items are valid by this definition.
+
+        .. code-block:: python
+
+            {
+                'items': [
+                    {'type': 'integer'},
+                    {'type': 'string'},
+                ],
+            }
+
+        Valid arrays are those with integers or strings, nothing else.
+
+        Since draft 06 definition can be also boolean. True means nothing, False
+        means everything is invalid.
+        """
+        items_definition = self._definition['items']
+        if items_definition is True:
+            return
+
         self.create_variable_is_list()
         with self.l('if {variable}_is_list:'):
             self.create_variable_with_length()
-            if isinstance(self._definition['items'], list):
-                for idx, item_definition in enumerate(self._definition['items']):
+            if items_definition is False:
+                with self.l('if {variable}:'):
+                    self.l('raise JsonSchemaException("{name} must not be there")')
+            elif isinstance(items_definition, list):
+                for idx, item_definition in enumerate(items_definition):
                     with self.l('if {variable}_len > {}:', idx):
                         self.l('{variable}_{0} = {variable}[{0}]', idx)
                         self.generate_func_code_block(
@@ -295,19 +319,19 @@ class CodeGeneratorDraft04(CodeGenerator):
 
                 if 'additionalItems' in self._definition:
                     if self._definition['additionalItems'] is False:
-                        self.l('if {variable}_len > {}: raise JsonSchemaException("{name} must contain only specified items")', len(self._definition['items']))
+                        self.l('if {variable}_len > {}: raise JsonSchemaException("{name} must contain only specified items")', len(items_definition))
                     else:
-                        with self.l('for {variable}_x, {variable}_item in enumerate({variable}[{0}:], {0}):', len(self._definition['items'])):
+                        with self.l('for {variable}_x, {variable}_item in enumerate({variable}[{0}:], {0}):', len(items_definition)):
                             self.generate_func_code_block(
                                 self._definition['additionalItems'],
                                 '{}_item'.format(self._variable),
                                 '{}[{{{}_x}}]'.format(self._variable_name, self._variable),
                             )
             else:
-                if self._definition['items']:
+                if items_definition:
                     with self.l('for {variable}_x, {variable}_item in enumerate({variable}):'):
                         self.generate_func_code_block(
-                            self._definition['items'],
+                            items_definition,
                             '{}_item'.format(self._variable),
                             '{}[{{{}_x}}]'.format(self._variable_name, self._variable),
                         )
