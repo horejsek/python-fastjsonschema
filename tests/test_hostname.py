aca2aaa2a3f81f6e2517fac1df81d280065c896d
Frederik Petersen <fp@abusix.com>
2018-09-12 17:24:16 +0200

    Switched to different hostname regex as this one did not allow for 2 char tlds. Fixes #26

--- tests/test_hostname.py
+++ tests/test_hostname.py
@@ -6,9 +6,14 @@ from fastjsonschema import JsonSchemaException
 exc = JsonSchemaException('data must be hostname')
 @pytest.mark.parametrize('value, expected', [
     ('', exc),
+    ('LDhsjf878&d', exc),
+    ('bla.bla-', exc),
+    ('example.example.com-', exc),
     ('localhost', 'localhost'),
     ('example.com', 'example.com'),
     ('example.de', 'example.de'),
+    ('example.fr', 'example.fr'),
+    ('example.example.com', 'example.example.com'),
 ])
 def test_datetime(asserter, value, expected):
     asserter({'type': 'string', 'format': 'hostname'}, value, expected)
