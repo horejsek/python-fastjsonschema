b6c4850b9b54fd8fd4cfbcd37222c2396cec81b3
Michal Horejsek <horejsekmichal@gmail.com>
2018-09-14 15:01:39 +0000

    Merged format tests into one file

--- tests/test_format.py
+++ tests/test_format.py
@@ -3,6 +3,17 @@ import pytest
 from fastjsonschema import JsonSchemaException
 
 
+exc = JsonSchemaException('data must be date-time')
+@pytest.mark.parametrize('value, expected', [
+    ('', exc),
+    ('bla', exc),
+    ('2018-02-05T14:17:10.00Z', '2018-02-05T14:17:10.00Z'),
+    ('2018-02-05T14:17:10Z', '2018-02-05T14:17:10Z'),
+])
+def test_datetime(asserter, value, expected):
+    asserter({'type': 'string', 'format': 'date-time'}, value, expected)
+
+
 exc = JsonSchemaException('data must be hostname')
 @pytest.mark.parametrize('value, expected', [
     ('', exc),
