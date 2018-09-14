eb346dc11610bf57cf8826b41662de2d212f155c
Frederik Petersen <fp@abusix.com>
2018-09-14 09:48:29 +0200

    Added length limit to hostname labels in regex and fixed test name method

--- tests/test_hostname.py
+++ tests/test_hostname.py
@@ -15,5 +15,5 @@ exc = JsonSchemaException('data must be hostname')
     ('example.fr', 'example.fr'),
     ('example.example.com', 'example.example.com'),
 ])
-def test_datetime(asserter, value, expected):
+def test_hostname(asserter, value, expected):
     asserter({'type': 'string', 'format': 'hostname'}, value, expected)
